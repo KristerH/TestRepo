0797fbef871530c73346b07e6a88ac53699db645
KristerOchNicklas <krister.hellsing@gmail.com>
2013-11-16 22:23:17 +0100

    La in en koppling i MVC-projektet till WCF-tjänsten.

---
+++ BasicMvcApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BasicMvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: //
+
+        public ActionResult Index()
+        {
+            BasicWCF.Service1Client client = new BasicWCF.Service1Client();
+            var buildings = client.GetAllBuildings();
+            client.Close();
+
+            return View();
+        }
+
+    }
+}
