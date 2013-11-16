dd1c312cc0afc70a604beeea2211239305f6df94
KristerOchNicklas <krister.hellsing@gmail.com>
2013-11-17 00:30:22 +0100

    Lekt med Dropdown

--- BasicMvcApp/Controllers/HomeController.cs
+++ BasicMvcApp/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BasicMvcApp.BasicWCF;
+using BasicMvcApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,14 +12,17 @@ namespace BasicMvcApp.Controllers
     {
         //
         // GET: //
-
+        [HttpGet]
         public ActionResult Index()
         {
             BasicWCF.Service1Client client = new BasicWCF.Service1Client();
             var buildings = client.GetAllBuildings();
             client.Close();
-
-            return View();
+            
+            Anmalan anmalan = new Anmalan();
+//            anmalan.BuildingList = new List<Building>(buildings);
+        
+            return View(anmalan);
         }
 
     }
---
+++ BasicMvcApp/Models/Anmalan.cs
@@ -0,0 +1,52 @@
+﻿using BasicMvcApp.BasicWCF;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace BasicMvcApp.Models
+
+{
+
+    public class Contrib
+    {
+        public int ContribId { get; set; }
+        public string Value { get; set; }
+    }
+
+
+
+
+    public class Anmalan
+    {
+
+        public class Contrib
+        {
+            public int ContribId { get; set; }
+            public string Value { get; set; }
+        }
+
+        public IEnumerable<Contrib> ContribTypeOptions =
+            new List<Contrib>
+        {
+            new Contrib {ContribId = 0, Value = "Payroll Deduction"},
+            new Contrib {ContribId = 1, Value = "Bill Me"}
+        };
+
+        [DisplayName("Contribution Type")]
+        public string ContribType { get; set; }
+
+        //public IEnumerable<Building> BuildinListOptions = new List<Building>
+        //{
+        //    new Building {Building_Code = "0", Description = "Payroll Deduction"},
+        //    new Building {Building_Code = "1", Description = "Bill Me"}
+        //};
+
+            
+            //BuildingList;
+
+        public List<Building> BuildingList  { get; set; } 
+        
+    }
+}
